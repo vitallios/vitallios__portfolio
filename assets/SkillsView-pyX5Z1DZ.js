1693f347a21c604815dc0978ed60851ab9928440
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-06-13 15:20:24 +0300

    Updates

---
+++ assets/SkillsView-pyX5Z1DZ.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-CnilsjNR.js";const s={},t={class:"container mx-auto px-4"};function n(r,a){return o(),c("div",t," Skills ")}const i=e(s,[["render",n]]);export{i as default};
