6e0ee6b0857ebb88e52d442c30bbbee86c752b50
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-06-13 11:27:29 +0300

    update all

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="Vitallios Portfolio" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
