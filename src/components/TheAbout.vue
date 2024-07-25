bf7d8e5c0982dfae3bbbeffcbb246dd978663fe7
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-07-25 14:14:40 +0300

    first commit

--- src/components/TheAbout.vue
+++ src/components/TheAbout.vue
@@ -1,8 +1,51 @@
 <script setup>
+const skills = [
+ {
+  id: 'frontend1',
+  title: '',
+  img: 'src/assets/image/gurl.jpg',
+  descrip: '',
+ },
+ {
+  id: 'frontend2',
+  title: '',
+  img: 'src/assets/image/gurl.jpg',
+  descrip: '',
+ },
+ {
+  id: 'frontend3',
+  title: '',
+  img: 'src/assets/image/gurl.jpg',
+  descrip: '',
+ },
+ {
+  id: 'frontend4',
+  title: '',
+  img: 'src/assets/image/gurl.jpg',
+  descrip: '',
+ },
+ {
+  id: 'frontend5',
+  title: '',
+  img: 'src/assets/image/gurl.jpg',
+  descrip: '',
+ },
 
+]
 </script>
 <template>
- <div>
-  <h1>A B O U T</h1>
+ <div id="about" class=" my-16">
+  <ul class="grid grid-cols-4 grid-row-2  gap-5">
+   <li 
+   v-for="(item, i) of skills" 
+   :key="item.id" 
+   class="rounded  overflow-hidden h-[300px]"  
+   :class="(`${i}` == '1' || `${i}` == '3' || `${i}` == 4) ? ' col-span-2' : ''"
+   >
+     <h2>{{ item.title }}</h2>
+     <img :src=item.img alt="Image" class=" w-full h-full bg-cover object-cover">
+     <p>{{ item.descrip }}</p>
+   </li>
+  </ul>
  </div>
 </template>
--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,11 +1,11 @@
 <script setup>
 
 </script>
-<template id="Home">
- <div class="flex  justify-center">
-  <div id="HomeTitle" class=" fle">
-   <h1>V I T A L L I O S</h1>
-   <h2>w e b  d e s i g n  &  f r o n t - e n d</h2>
+<template id="Home" class="max-h-[700px]">
+ <div class="flex justify-center my-48">
+  <div id="HomeTitle" class="flex justify-center flex-col h-full text-center">
+   <h1 class=" w-full  text-[2.3em]  sm:text-[8vw] tracking-widest">V I T A L L I O S</h1>
+   <h2 class="flex gap-4 justify-center "><span class=" tracking-widest">w e b  d e s i g n</span><span class=" tracking-widest">&</span><span class=" tracking-widest">f r o n t - e n d</span></h2>
   </div>
  </div>
 </template>
